b290f29c5a21a2aedc5b36cb74223df57d9825fb
Adam <adamtemplin92@gmail.com>
2023-06-09 13:47:34 +0200

    TermNetTask - Add TicTacToeGame class, migrate part of functinoality, add new tests

--- tests.py
+++ tests.py
@@ -3,74 +3,120 @@ import tic_tac_toe as ttt
 
 class TestTicTacToe(unittest.TestCase):
 
-    def test_new_game(self):
-        ticTacToe = ttt.TicTacToe()
-        board = ticTacToe.get_board()
+    def test_empty_board(self):
+        ttt_board = ttt.TicTacToeBoard()
+        board = ttt_board.get_board()
         for i in range(3):
             for j in range(3):
                 self.assertIsNone(board[i][j])
+
+    def test_set_symbol(self):
+        ttt_board = ttt.TicTacToeBoard()
+        ttt_board.set_symbol(row=1, col=2, symbol='O')
+        ttt_board.set_symbol(row=2, col=1, symbol='X')
+        expected_board = [[None, None, None], [None, None, 'O'], [None, 'X', None]]
+        self.assertEqual(expected_board, ttt_board.get_board())
+    
+    def test_set_symbol_wrong_symbol(self):
+        ttt_board = ttt.TicTacToeBoard()
+        with self.assertRaises(AttributeError):
+            ttt_board.set_symbol(row=1, col=2, symbol='0')
+        
+    def test_set_symbol_out_of_range(self):
+        ttt_board = ttt.TicTacToeBoard()
+        with self.assertRaises(IndexError):
+            ttt_board.set_symbol(row=1, col=3, symbol='O')
+
+    def test_set_symbol_existing_symbol(self):
+        ttt_board = ttt.TicTacToeBoard()
+        ttt_board.set_symbol(row=1, col=2, symbol='O')
+        with self.assertRaises(AttributeError):
+            ttt_board.set_symbol(row=1, col=2, symbol='X')
         
     def test_first_player(self):
-        ticTacToe = ttt.TicTacToe()
-        current_player = ticTacToe.get_current_player()
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
         self.assertTrue(current_player in ['O', 'X'])
     
     def test_player_change(self):
-        ticTacToe = ttt.TicTacToe()
-        current_player = ticTacToe.get_current_player()
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
         players = ['O', 'X']
         players.pop(players.index(current_player))
-        ticTacToe.change_player()
-        current_player = ticTacToe.get_current_player()
+        ttt_game.change_player()
+        current_player = ttt_game.get_current_player()
         self.assertEqual(players[0], current_player)
 
-    def test_set_symbol(self):
-        ticTacToe = ttt.TicTacToe()
-        ticTacToe.set_symbol(x=1, y=2, symbol='O')
-        ticTacToe.set_symbol(x=2, y=1, symbol='X')
-        expected_board = [[None, None, None], [None, None, 'O'], [None, 'X', None]]
-        self.assertEqual(expected_board, ticTacToe.get_board())
+    def test_check_winner_row(self):
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
+        for i in range(3):
+            ttt_game.player_move(row=0, col=i)
+        self.assertEqual(current_player, ttt_game.check_winner())
+
+    def test_check_winner_column(self):
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
+        for i in range(3):
+            ttt_game.player_move(row=i, col=1)
+        self.assertEqual(current_player, ttt_game.check_winner())
+
+    def test_check_winner_axis_1(self):
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
+        for i in range(3):
+            ttt_game.player_move(row=i, col=i)
+        self.assertEqual(current_player, ttt_game.check_winner())
+
+    def test_check_winner_axis_2(self):
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
+        for i in range(3):
+            ttt_game.player_move(row=2-i, col=i)
+        self.assertEqual(current_player, ttt_game.check_winner())
     
-    def test_set_symbol_wrong_symbol(self):
-        ticTacToe = ttt.TicTacToe()
-        with self.assertRaises(AttributeError):
-            ticTacToe.set_symbol(x=1, y=2, symbol='0')
-        
-    def test_set_symbol_out_of_range(self):
-        ticTacToe = ttt.TicTacToe()
-        with self.assertRaises(IndexError):
-            ticTacToe.set_symbol(x=1, y=3, symbol='O')
+    def test_check_winner_no_winner(self):
+        ttt_game = ttt.TicTacToeGame()
+        self.assertIsNone(ttt_game.check_winner())
 
-    def test_set_symbol_existing_symbol(self):
-        ticTacToe = ttt.TicTacToe()
-        ticTacToe.set_symbol(x=1, y=2, symbol='O')
-        with self.assertRaises(AttributeError):
-            ticTacToe.set_symbol(x=1, y=2, symbol='X')
+    def tect_check_move_exist(self):
+        ttt_game = ttt.TicTacToeGame()
+        self.assertTrue(ttt_game.check_move())
 
-
-    def test_winner_row(self):
-        ticTacToe = ttt.TicTacToe()
+    def test_check_no_move_exist(self):
+        ttt_game = ttt.TicTacToeGame()
         for i in range(3):
-            ticTacToe.set_symbol(x=0, y=i, symbol='O')
-        self.assertEqual('O', ticTacToe.check_winner())
+            for j in range(3):
+                ttt_game.player_move(i, j)
+        self.assertFalse(ttt_game.check_move())
 
-    def test_winner_column(self):
-        ticTacToe = ttt.TicTacToe()
+    def test_next_round_winner(self):
+        ttt_game = ttt.TicTacToeGame()
+        current_player = ttt_game.get_current_player()
         for i in range(3):
-            ticTacToe.set_symbol(x=i, y=1, symbol='X')
-        self.assertEqual('X', ticTacToe.check_winner())
+            ttt_game.player_move(row=2-i, col=i)
+        next_move, winner = ttt_game.next_round()
+        self.assertEqual([False, current_player], [next_move, winner])
 
-    def test_winner_axis_1(self):
-        ticTacToe = ttt.TicTacToe()
-        for i in range(3):
-            ticTacToe.set_symbol(x=i, y=i, symbol='O')
-        self.assertEqual('O', ticTacToe.check_winner())
+    def test_next_round_no_winner(self):
+        ttt_game = ttt.TicTacToeGame()
+        next_move, winner = ttt_game.next_round()
+        self.assertEqual([True, None], [next_move, winner])
 
-    def test_winner_axis_2(self):
-        ticTacToe = ttt.TicTacToe()
-        for i in range(3):
-            ticTacToe.set_symbol(x=2-i, y=i, symbol='X')
-        self.assertEqual('X', ticTacToe.check_winner())
-    
+    def test_next_round_tie(self):
+        ttt_game = ttt.TicTacToeGame()
+        ttt_game.player_move(row=0, col=0)
+        ttt_game.player_move(row=0, col=1)
+        ttt_game.player_move(row=1, col=2)
+        ttt_game.player_move(row=2, col=0)
+        ttt_game.player_move(row=2, col=2)
+        ttt_game.change_player()
+        ttt_game.player_move(row=0, col=2)
+        ttt_game.player_move(row=1, col=0)
+        ttt_game.player_move(row=1, col=1)
+        ttt_game.player_move(row=2, col=1)
+        next_move, winner = ttt_game.next_round()
+        self.assertEqual([False, None], [next_move, winner])
+
 if __name__ == '__main__':
     unittest.main()
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,51 +1,99 @@
 from typing import Literal
 import random
 
-class TicTacToe():
-    players = ['O', 'X']
+class TicTacToeBoard():
 
-    def __init__(self):
-        self.create_board()
-        self.first_player = random.sample(['O', 'X'], 1)[0]
-        self.current_player = self.first_player
+    def __init__(self, board=None) -> None:
+        if board:
+            self.board = board
+        else:
+            self.create_board()
 
     def create_board(self) -> None:
         self.board = [[None, None, None], [None, None, None], [None, None, None]]
 
-    def get_current_player(self) -> Literal['O', 'X']:
-        return self.current_player
-    
-    def change_player(self):
-        current_player_index = self.players.index(self.current_player)
-        self.current_player = self.players[1 - current_player_index]
-
     def get_board(self) -> None:
         return self.board
     
-    def set_symbol(self, x: Literal[0,1,2], y: Literal[0,1,2], 
+    def set_symbol(self, row: Literal[0,1,2], col: Literal[0,1,2], 
                    symbol: Literal['O', 'X']) -> None:
         if symbol not in ['O', 'X']:
             raise AttributeError(f"Symbol {symbol} not in ['O', 'X']")
-        if self.get_symbol(x, y):
-            raise AttributeError(f"Field [{x}, {y}] already set to \
-                                {self.get_symbol(x, y)}")
-        self.board[x][y] = symbol
+        if self.get_symbol(row, col):
+            raise AttributeError(f"Field [{row}, {col}] already set to \
+                                {self.get_symbol(row, col)}")
+        self.board[row][col] = symbol
     
-    def get_symbol(self, x: Literal[0,1,2], y: Literal[0,1,2]) -> Literal['O', 'X', None]:
-        return self.board[x][y]
+    def get_symbol(self, row: Literal[0,1,2], col: Literal[0,1,2]) -> Literal['O', 'X', None]:
+        return self.board[row][col]
+        
+class TicTacToeGame():
+    players = ['O', 'X']
     
+    def __init__(self, first_player:Literal['O', 'X']=None) -> None:
+        self.new_game(first_player=first_player)
+    
+    def new_game(self, first_player:Literal['O', 'X']=None) -> None:
+        self.tic_tac_toe_board = TicTacToeBoard()
+        self.round = 1
+        if first_player:
+            if first_player in ['O', 'X']:
+                self.first_player = first_player
+            else:
+                raise AttributeError((f"Player {first_player} not in ['O', 'X']"))
+        else: 
+            self.first_player = random.sample(['O', 'X'], 1)[0]
+        self.current_player = self.first_player
+    
+    def load_game(self, board:TicTacToeBoard, current_player:Literal['X', 'O']) -> None:
+        self.tic_tac_toe_board = board
+        self.current_player = current_player
+
+    def get_current_player(self) -> Literal['O', 'X']:
+        return self.current_player
+    
+    def change_player(self) -> None:
+        current_player_index = self.players.index(self.current_player)
+        self.current_player = self.players[1 - current_player_index]
+
+    def player_move(self, row: Literal[0,1,2], col: Literal[0,1,2]):
+        self.tic_tac_toe_board.set_symbol(row=row, col=col, symbol=self.current_player)
+
+    def check_move(self):
+        move_exists = False
+        board = self.tic_tac_toe_board.get_board()
+        for i in range(3):
+            move_exists = move_exists or None in board[i]
+        return move_exists
+            
     def check_winner(self) -> Literal['O', 'X', None]:
         for i in range(3):
-            if self.get_symbol(i, 0) == self.get_symbol(i, 1) == self.get_symbol(i, 2):
-                if self.get_symbol(i, 0):
-                    return self.get_symbol(i, 0)
-            if self.get_symbol(0, i) == self.get_symbol(1, i) == self.get_symbol(2, i):
-                if self.get_symbol(0, i):
-                    return self.get_symbol(0, i)
-        if self.get_symbol(0, 0) == self.get_symbol(1, 1) == self.get_symbol(2, 2):
-            if self.get_symbol(0, 0):
-                return self.get_symbol(0, 0)
-        if self.get_symbol(0, 2) == self.get_symbol(1, 1) == self.get_symbol(2, 0):
-            if self.get_symbol(0, 2):
-                return self.get_symbol(0, 2)
+            if self.tic_tac_toe_board.get_symbol(i, 0) == \
+                    self.tic_tac_toe_board.get_symbol(i, 1) == \
+                    self.tic_tac_toe_board.get_symbol(i, 2):
+                if self.tic_tac_toe_board.get_symbol(i, 0):
+                    return self.tic_tac_toe_board.get_symbol(i, 0)
+            if self.tic_tac_toe_board.get_symbol(0, i) == \
+                    self.tic_tac_toe_board.get_symbol(1, i) == \
+                    self.tic_tac_toe_board.get_symbol(2, i):
+                if self.tic_tac_toe_board.get_symbol(0, i):
+                    return self.tic_tac_toe_board.get_symbol(0, i)
+        if self.tic_tac_toe_board.get_symbol(0, 0) == \
+                self.tic_tac_toe_board.get_symbol(1, 1) == \
+                self.tic_tac_toe_board.get_symbol(2, 2):
+            if self.tic_tac_toe_board.get_symbol(0, 0):
+                return self.tic_tac_toe_board.get_symbol(0, 0)
+        if self.tic_tac_toe_board.get_symbol(0, 2) == \
+                self.tic_tac_toe_board.get_symbol(1, 1) == \
+                self.tic_tac_toe_board.get_symbol(2, 0):
+            if self.tic_tac_toe_board.get_symbol(0, 2):
+                return self.tic_tac_toe_board.get_symbol(0, 2)
+        return None
+    
+    def next_round(self):
+        winner = self.check_winner()
+        next_move = self.check_move() if not winner else False
+        self.change_player()
+        return next_move, winner
+
         
